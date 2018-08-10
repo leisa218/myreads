4d3eaf0ba95b30c94caac20d408c26b0bef3959d
Natalie Klinke <nk@10vorne.it>
2018-08-10 14:25:28 +0200

    making chages in Router definition to make site run on github page

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
+import createHashHistory from 'history/createHashHistory';
 import App from './App'
 import './index.css'
 
+const hashHistory = createHashHistory({ basename: process.env.PUBLIC_URL });
+
 ReactDOM.render(
-	<BrowserRouter>
+	<BrowserRouter history={hashHistory}>
 	<App />
 	</BrowserRouter>, 
 	document.getElementById('root')
